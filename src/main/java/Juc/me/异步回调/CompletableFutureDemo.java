6a94dfd33f68e4ef425e0c5239feb68c48ac804e
jizhideluyi <zhangconglong123@163.com>
2022-06-08 15:44:31 +0800

    <feat>:异步回调

---
+++ src/main/java/Juc/me/异步回调/CompletableFutureDemo.java
@@ -0,0 +1,29 @@
+package Juc.me.异步回调;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * 异步回调
+ * author: luyi
+ * time: 2022/3/11 22:11
+ */
+public class CompletableFutureDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //异步回调 没有返回值
+        CompletableFuture<Void> complete = CompletableFuture.runAsync(()->{
+            System.out.println(Thread.currentThread().getName());
+        });
+        complete.get();
+
+        //异步回调 有返回值supply
+        CompletableFuture<Integer> complete2 = CompletableFuture.supplyAsync(()->{
+            System.out.println(Thread.currentThread().getName());
+            return 12;
+        });
+        complete.whenComplete((t, u)->{
+            System.out.println("t"+t);//方法的返回值
+            System.out.println("u"+u);//异常信息
+        }).get();
+    }
+}
