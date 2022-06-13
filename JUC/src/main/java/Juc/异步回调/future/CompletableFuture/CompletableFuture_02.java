5b975ae7e54a84543fcd6d6d73a38c8ea3c48674
jizhideluyi <zhangconglong123@163.com>
2022-06-13 22:13:48 +0800

    <feat>:CompletableFuture的默认构造方法

--- JUC/src/main/java/Juc/异步回调/future/CompletableFuture/CompletableFuture_02.java
+++ JUC/src/main/java/Juc/异步回调/future/CompletableFuture/CompletableFuture_02.java
@@ -7,27 +7,22 @@ import java.util.concurrent.*;
  */
 public class CompletableFuture_02 {
 
-    //创建全局线程池
-    static ThreadPoolExecutor executor = new ThreadPoolExecutor(5,
-            50,
-            10,//空闲存活时间
-            TimeUnit.SECONDS,
-            new LinkedBlockingDeque<>(100),//任务队列
-            Executors.defaultThreadFactory(),//设置工厂
-            new ThreadPoolExecutor.AbortPolicy() //设置拒绝策略
-    );
-
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println("主线程start...");
 
+
+        /*
+         * 不指定线程池默认为【ForkJoinPool.commonPool】
+         */
         CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
             System.out.println("任务2，子线程：" + Thread.currentThread().getName());
             int i = 10/0;
             return 10;//发生异常，失效
-        }, executor);
+        });
 
-        //注意：上面不能分开
-        //register
+        
+        
+        //阻塞
         System.out.println("主线程end..."+future2.get());
     }
 }
