cb284974b309635885477141043eb3b097bb312d
jizhideluyi <zhangconglong123@163.com>
2022-06-19 23:00:02 +0800

    <feat>: BigDecimal用于金额比较

---
+++ jdk/src/main/java/Math/BigDecimal/BigDecimal_demo.java
@@ -0,0 +1,31 @@
+package Math.BigDecimal;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+
+/**
+ * @author: luyi
+ * time: 2022/6/19 22:47
+ */
+@Slf4j
+public class BigDecimal_demo {
+
+    public static void main(String[] args) {
+//        BigDecimal bigDecimal = BigDecimal.ONE;
+        BigDecimal bigDecimal = new BigDecimal("1.0");
+        BigDecimal bigDecimal1 = new BigDecimal("1.00");
+        
+        System.out.println( "equals: " + bigDecimal.equals(bigDecimal1)  );
+        System.out.println( "compareTo: " + (bigDecimal.compareTo(bigDecimal1)==0) );
+
+
+        /* 比较2.0和2.00
+         *  （1）equals 不等
+         *      无法用于金额比较（类似String）
+         *  （2）compareTo 相等
+         *      >0 第一个大于第二个
+         *      <0 第一个小于第二个    
+         */
+    }
+}
