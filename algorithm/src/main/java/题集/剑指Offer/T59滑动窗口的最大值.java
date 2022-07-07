7c68a3c0457ec13742ca8c86d57544bf1d2a2145
张从龙 <zhangconglong123@163.com>
2022-07-08 00:45:06 +0800

    <feat>: T59滑动窗口的最大值

--- algorithm/src/main/java/题集/剑指Offer/T59滑动窗口的最大值.java
+++ algorithm/src/main/java/题集/剑指Offer/T59滑动窗口的最大值.java
@@ -1,8 +1,35 @@
 package 题集.剑指Offer;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @author zhangconglong
  * @version [1.0.0, 2022/7/8]
  */
 public class T59滑动窗口的最大值 {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int[] res = new int[nums.length - k + 1];
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        for(int i = 0, j = 0; i < nums.length; i++) {
+            if(!queue.isEmpty() && i - queue.peek() >= k) {
+                queue.poll();
+            }
+            while(!queue.isEmpty() && nums[i] > nums[queue.peekLast()]) {
+                queue.pollLast();
+            }
+            queue.offer(i);
+            if(i >= k - 1) {
+                res[j++] = nums[queue.peek()];
+            }
+        }
+
+        return res;
+    }
+
 }
