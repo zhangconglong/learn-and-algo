e22e179a202933e497aceff27c82de93af139aa4
张从龙 <zhangconglong123@163.com>
2022-07-11 15:10:56 +0800

    <feat>: 使用Disruptor的案例

---
+++ JUC/src/main/java/Juc/内存队列/Disruptor/MyThreadFactory.java
@@ -0,0 +1,17 @@
+package Juc.内存队列.Disruptor;
+
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * @author zhangconglong
+ * @version [1.0.0, 2022/7/11]
+ */
+public class MyThreadFactory implements ThreadFactory {
+
+    @Override
+    public Thread newThread(Runnable r) {
+        int i = 0;
+
+        return new Thread(r, "生产者线程simpleThread" + i++);
+    }
+}
