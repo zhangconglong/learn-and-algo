b777150965fe2bdc76b72ac54d7d6d7f719cefc8
张从龙 <zhangconglong123@163.com>
2022-07-12 14:35:15 +0800

    <feat>: JDK final的赋值时机

---
+++ jdk/src/main/java/修饰符/final_/Final赋值时机.java
@@ -0,0 +1,31 @@
+package 修饰符.final_;
+
+/**
+ * @author zhangconglong
+ * @version [1.0.0, 2022/7/12]
+ */
+public class Final赋值时机 {
+    //一、声明时赋值
+    private final int val_1 = 10;
+
+
+    private final int val_2;
+    /**
+     * 构造函数
+     */
+    Final赋值时机(){
+        val_2 = 2;
+    }
+
+    private final int val_3;
+
+    {
+        val_3 = 10;
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("world");
+    }
+}
