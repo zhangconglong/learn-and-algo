0310bd8beb227238e2c3f9e6e2594b98c5646bd7
张从龙 <zhangconglong123@163.com>
2022-07-12 11:15:38 +0800

    <feat>: 定义Event事件

---
+++ JUC/src/main/java/Juc/内存队列/无界队列/Disruptor/Demo/LongEvent.java
@@ -0,0 +1,13 @@
+package Juc.内存队列.无界队列.Disruptor.Demo;
+
+public class LongEvent {
+    private long value;
+
+    public long getValue() { 
+        return value; 
+    } 
+ 
+    public void setValue(long value) { 
+        this.value = value; 
+    } 
+} 
