88f732d68d8d1ff69b95bef18564d0b6e3f09f0a
张从龙 <zhangconglong123@163.com>
2022-07-14 22:28:22 +0800

    <feat>: 动态规划- 斐波那契数列

---
+++ algorithm/src/main/java/数据结构与算法/动态规划/斐波那契/Main.java
@@ -0,0 +1,28 @@
+package 数据结构与算法.动态规划.斐波那契;
+
+import java.util.Scanner;
+
+/**
+ * https://www.nowcoder.com/practice/ee5d403c1172487f8c7915b3c3d924c6
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        short n = in.nextShort();
+    
+        System.out.println(fibo(n));
+    }
+    
+    public static int fibo(short n){
+        int res = 0;
+        if(n==1){
+            res = 1;
+        }else if(n==2){
+            res = 1;
+        }else { //大于2
+            res = fibo((short) (n-1))+fibo((short) (n-2));
+        }
+        
+        return res;
+    }
+}
