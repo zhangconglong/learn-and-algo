7b1961cb01eb3993fef89eacdcd9f8e4ea207b70
张从龙 <zhangconglong123@163.com>
2022-07-23 14:26:27 +0800

    <feat>：final修饰符

---
+++ jdk/src/main/java/修饰符/final_/Final_abstract_demo.java
@@ -0,0 +1,13 @@
+//package 修饰符.final_;
+//
+///**
+// * @author zhangconglong
+// * @version [1.0.0, 2022/7/12]
+// */
+//public class Final_abstract_demo extends Final_abstract_demo_1{
+//
+//    @Override
+//    public final void test(){}
+//    String
+//
+//}
---
+++ jdk/src/main/java/修饰符/final_/Final_abstract_demo_1.java
@@ -0,0 +1,16 @@
+package 修饰符.final_;
+
+import java.util.stream.Stream;
+
+/**
+ * @author zhangconglong
+ * @version [1.0.0, 2022/7/12]
+ */
+public abstract  class Final_abstract_demo_1 {
+
+    public final void test(){}
+
+    public final void test(Stream name){}
+
+
+}
